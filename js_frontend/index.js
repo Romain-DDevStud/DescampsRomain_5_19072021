e18229b48198610c59b3d634add15680cf6ba1c6
Romain Descamps <descampsr@hotmail.fr>
2021-08-07 19:08:56 +0200

    update homepage ok

---
+++ js_frontend/index.js
@@ -0,0 +1,39 @@
+main()
+
+function main() {
+    getArticles();
+}
+// Fonction pour récupérer la liste des articles
+function getArticles() {
+    fetch("http://localhost:3000/api/cameras")
+        .then(function(res){
+            if (res.ok){
+                return res.json();
+            }
+        })
+        .then(function(value){
+            const articles = value;
+            let template = document.querySelector("#article");
+            let section = document.querySelector(".product__list");
+            for (const article of articles){ // Récupération des informations produits
+                let clone = document.importNode(template.content, true);
+                let url = clone.querySelector(".product__URL");
+                url.href = "../product.html?id=" + article._id;
+                let image = clone.querySelector(".product__img");
+                image.src = article.imageUrl;
+                let name = clone.querySelector(".product__name");
+                name.innerHTML = article.name;
+                let price = clone.querySelector(".product__price");
+                price.innerHTML = article.price;
+                section.appendChild(clone);
+                article.price = article.price / 100; // Modification du format d'affichage du prix
+                price.innerHTML = new Intl.NumberFormat("fr-FR",{ 
+                    style: "currency",
+                    currency: "EUR",
+                })
+                .format(article.price);
+            }
+        })
+        .catch(function(err){
+        });
+}
